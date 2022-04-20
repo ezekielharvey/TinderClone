eb68b41fc2232bef79e60d97ddfe3dbbc652b085
ezekielharvey <zekeharvey@outlook.com>
2022-04-20 13:54:56 -0400

    Home Screen and CHat Screen

---
+++ StackNavigator.js
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import { Text, View } from 'react-native'
+import HomeScreen from './Screens/HomeScreen'
+import ChatScreen from './Screens/ChatScreen'
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='Chat' component={ChatScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export default StackNavigator;
+
